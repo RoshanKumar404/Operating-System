22f3dcacd14325f1abacc7f29fc7de4c129adaf3
Roshankumar404 <roshankuar951@gmail.com>
2025-11-13 16:42:45 +0530

    creating this repo for the os things

---
+++ IPCviaPipeline.java
@@ -0,0 +1,33 @@
+ import java.io.*;
+ public class IPCviaPipeline {
+   public static void main(String[] args)throws IOException,InterruptedException {
+    // creating the  first process
+    ProcessBuilder Process1= new ProcessBuilder("cat","IPCviaPipeline.java");
+   Process pb1=Process1.start();
+    // creating the second process
+    ProcessBuilder Process2= new ProcessBuilder("grep","JaiSriRam");
+    Process pb2= Process2.start();
+
+    // after creating the processes  now lets connect them 
+    try(
+        InputStream pb1output=pb1.getInputStream();
+        OutputStream pb2Output= pb2.getOutputStream();
+
+    ){
+        pb1output.transferTo(pb2Output);
+        
+    }
+    //closing the input to signal end of stream
+    pb2.getOutputStream().close();
+    //printing greps output  Grep:- this commands used to search the keywords in file using terminal or shell
+    pb2.getInputStream().transferTo(System.out);
+    
+
+    // and lets wait for both processes to finish
+    pb1.waitFor();
+    pb2.waitFor();
+    
+   }
+}
+
+
